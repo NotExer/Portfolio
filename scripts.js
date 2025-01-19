c86855f5e99e40b8a3de9ec04454306b8bff175c
NotExer <samurpo20@gmail.com>
2025-01-19 12:37:35 -0500

    0.1

---
+++ scripts.js
@@ -0,0 +1,68 @@
+function toggleMenu() {
+    const hamburger = document.querySelector('.hamburger');
+    const menus = document.querySelectorAll('.item, .item-show, .menu, .menu-show, .logo, .logo-show, .navbar, .navbar-show');
+    const body = document.body;  // Obtener el body
+  
+    // Alterna la clase activa en el botón de menú
+    hamburger.classList.toggle('active');
+  
+    // Alterna las clases de los elementos del menú con transición
+    menus.forEach(menu => {
+      ['item', 'menu', 'logo', 'navbar'].forEach(baseClass => {
+        if (menu.classList.contains(baseClass)) {
+          menu.classList.remove(baseClass);
+          menu.classList.add(`${baseClass}-show`);
+        } else if (menu.classList.contains(`${baseClass}-show`)) {
+          menu.classList.remove(`${baseClass}-show`);
+          menu.classList.add(baseClass);
+        }
+      });
+    });
+  
+    // Bloquear o permitir el scroll en el body
+    body.classList.toggle('no-scroll');
+    
+    // Aplicar transición de cierre al menú
+    menus.forEach(menu => {
+      if (menu.classList.contains('item-show')) {
+        menu.addEventListener('click', () => {
+          // Cierra el menú al seleccionar un título
+          closeMenu();
+        });
+      }
+    });
+  }
+  
+  // Cerrar el menú y restaurar clases con transición
+  const links = document.querySelectorAll('.navbar a');
+  links.forEach(link => {
+    link.addEventListener('click', () => {
+      closeMenu();
+    });
+  });
+  
+  function closeMenu() {
+    const hamburger = document.querySelector('.hamburger');
+    const menus = document.querySelectorAll('.item, .item-show, .menu, .menu-show, .logo, .logo-show, .navbar, .navbar-show');
+    const body = document.body;  // Obtener el body
+  
+    // Remover las clases de los elementos del menú con transición
+    menus.forEach(menu => {
+      ['item', 'menu', 'logo', 'navbar'].forEach(baseClass => {
+        if (menu.classList.contains(`${baseClass}-show`)) {
+          menu.classList.remove(`${baseClass}-show`);
+          // Activar la transición agregando la clase 'active'
+          setTimeout(() => {
+            menu.classList.add(baseClass);
+          }, 100); // Esto crea el retraso antes de restaurar las clases
+        }
+      });
+    });
+  
+    // Remover la clase activa en el botón de menú
+    hamburger.classList.remove('active');
+  
+    // Permitir el scroll en el body
+    body.classList.remove('no-scroll');
+  }
+  
