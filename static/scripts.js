8199929e9cdf40f3d70e9c0713cae60474f15cf7
NotExer <samurpo20@gmail.com>
2026-02-18 19:14:18 -0500

    maximizer

--- static/scripts.js
+++ static/scripts.js
@@ -209,7 +209,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("form");
   if (form) {
     form.addEventListener("submit", () => {
-      console.log("Form submitted");
     });
   }
 });
