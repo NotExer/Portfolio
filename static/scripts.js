3efcde7f53751e342d1374cedaa02aee5ae402eb
NotExer <samurpo20@gmail.com>
2026-02-18 19:52:07 -0500

    general fixes

--- static/scripts.js
+++ static/scripts.js
@@ -209,6 +209,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("form");
   if (form) {
     form.addEventListener("submit", () => {
-    });
+    }); 
   }
 });
